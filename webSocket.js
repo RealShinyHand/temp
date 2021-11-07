7a704082502b68dd47e01e152b819a131c706c5f
Son GwangJin <start_so@naver.com>
2021-11-07 17:41:52 +0900

    index.html에 실시간 스트리밍 추가 & 현재 접속자 출력

--- webSocket.js
+++ webSocket.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const { mobileData } = require('./socket');
 
-
+const currentUser = [];
 
 module.exports = (server, app, PubSub) => {
     const io = SocketIO(server, { path: '/socket.io' });
@@ -16,9 +16,14 @@ module.exports = (server, app, PubSub) => {
         const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log('새로운 클라이언트 접속!', ip, socket.id);
+        currentUser.push(ip);
+        io.emit('user', { currentUser});
         socket.on('disconnect', () => { // 연결 종료 시
             console.log('클라이언트 접속 해제', ip, socket.id);
             PubSub.unsubscribe(subscriber_token);
+            const index = currentUser.findIndex(el => el === ip);
+            currentUser.splice(index, 1);
+            io.emit('user', {currentUser});
         });
         socket.on('error', (error) => { // 에러 시
             console.error(error);
@@ -28,7 +33,7 @@ module.exports = (server, app, PubSub) => {
             socket.emit('data', {
                 temperature: temp,
                 humidity: humid,
-                decibel
+                decibel,
             });
         }, 3000);
     });
